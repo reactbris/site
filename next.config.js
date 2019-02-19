2454fc30fc1102d99df7d6876088f4d083abd77d
Lochlan Bunn <bunn@lochlan.io>
2019-02-19 13:16:52 +1000

    cmon

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@ const withCSS = require("@zeit/next-css");
 
 const config = withCSS();
 
-if (process.env.NOW_REGION) {
+console.log(JSON.stringify(process.env, null, 2));
+
+if (process.env.NODE_ENV === "production") {
   config.target = "serverless";
 }
 
